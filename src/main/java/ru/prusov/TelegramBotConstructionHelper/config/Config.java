db6774ef1acab3d3b4fa96d3be14b4b3fd2a2b8a
Артем Васильевич Прусов <itcoding76@gmail.com>
2025-08-04 21:57:50 +0200

    empty blank telegram bot

---
+++ src/main/java/ru/prusov/TelegramBotConstructionHelper/config/Config.java
@@ -0,0 +1,21 @@
+package ru.prusov.TelegramBotConstructionHelper.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+@Configuration
+public class Config {
+    @Bean
+    public String botToken(@Value("${bot.botToken}") String botToken){
+        return botToken;
+    }
+
+    @Bean
+    public TelegramClient getTelegramClient(@Autowired String botToken){
+        return new OkHttpTelegramClient(botToken);
+    }
+}
---
+++ src/main/java/ru/prusov/TelegramBotConstructionHelper/usecase/services/TelegramBotService.java
@@ -0,0 +1,42 @@
+package ru.prusov.TelegramBotConstructionHelper.usecase.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.longpolling.BotSession;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.AfterBotRegistration;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Slf4j
+@Service
+@EnableAsync
+@RequiredArgsConstructor
+public class TelegramBotService implements LongPollingSingleThreadUpdateConsumer, SpringLongPollingBot {
+    private final String botToken;
+
+    @Override
+    public void consume(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            log.info("it is working");
+        }
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return this;
+    }
+
+    @AfterBotRegistration
+    public void afterRegistration(BotSession botSession) {
+        log.info("Registered bot running state is: {}", botSession.isRunning());
+    }
+}
