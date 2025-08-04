ba014cb96b093a204fb2132e5a1295ab030391be
Артем Васильевич Прусов <itcoding76@gmail.com>
2025-08-04 21:49:38 +0200

    Initial commit

---
+++ src/main/java/ru/prusov/TelegramBotConstructionHelper/TelegramBotConstructionHelperApplication.java
@@ -0,0 +1,13 @@
+package ru.prusov.TelegramBotConstructionHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotConstructionHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotConstructionHelperApplication.class, args);
+	}
+
+}
