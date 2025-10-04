b9bfb8a011cc9390a2ae56eeb5b3e21f007bc6f7
Артем Васильевич Прусов <itcoding76@gmail.com>
2025-10-04 09:21:25 +0200

    refactoring track messageId in abstract class

--- src/main/java/ru/prusov/TelegramBotConstructionHelper/usecase/commands/AbstractCommand.java
+++ src/main/java/ru/prusov/TelegramBotConstructionHelper/usecase/commands/AbstractCommand.java
@@ -1,6 +1,7 @@
 package ru.prusov.TelegramBotConstructionHelper.usecase.commands;
 
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
@@ -11,12 +12,16 @@ import ru.prusov.TelegramBotConstructionHelper.dto.CommonInfo;
 import ru.prusov.TelegramBotConstructionHelper.factory.AnswerMethodFactory;
 import ru.prusov.TelegramBotConstructionHelper.usecase.services.MessageIDKeeperService;
 
+import java.util.List;
+
+
 public abstract class AbstractCommand implements Command {
+
     protected MessageIDKeeperService messageIDKeeperService;
     protected TelegramSender sender;
 
     @Autowired
-    protected void setDependencies(MessageIDKeeperService messageIDKeeperService, TelegramSender sender) {
+    protected void setDependency(MessageIDKeeperService messageIDKeeperService, TelegramSender sender) {
         this.messageIDKeeperService = messageIDKeeperService;
         this.sender = sender;
     }
@@ -26,7 +31,7 @@ public abstract class AbstractCommand implements Command {
         try {
             doExecute(commonInfo);
         } catch (Exception e) {
-            log().error("Command failed {} for chat {}", command(), commonInfo.getChatId(), e);
+            log().error("Command: {} failed {} for chat {}", getClass().getSimpleName(), command(), commonInfo.getChatId(), e);
             throw e;
         }
     }
@@ -36,7 +41,12 @@ public abstract class AbstractCommand implements Command {
     }
 
     protected void deleteAllMessage(Long chatId) {
-        sender.execute(new DeleteMessages(chatId.toString(), messageIDKeeperService.deleteAllMessageId(chatId)));
+        List<Integer> integers = messageIDKeeperService.deleteAllMessageId(chatId);
+        if (integers != null && !integers.isEmpty()) {
+            sender.execute(new DeleteMessages(chatId.toString(), integers));
+            log().info("Command: {} Deleted {} messages for chat {}", getClass().getSimpleName(), integers.size(), chatId);
+        }
+        log().info("Command: {} No messages to delete for chat {}", getClass().getSimpleName(), chatId);
     }
 
     protected void reply(Long chatId, String text, InlineKeyboardMarkup keyboardMarkup) {
@@ -48,10 +58,35 @@ public abstract class AbstractCommand implements Command {
     }
 
     protected void sendPhoto(Long chatId, String photoPath) {
-        SendPhoto sendPhoto = AnswerMethodFactory.getSendPhoto(chatId, photoPath);
-        sender.execute(sendPhoto);
+        sender.execute(AnswerMethodFactory.getSendPhoto(chatId, photoPath));
     }
 
+
+    protected void sendPhotoAndTrack(Long chatId, String photoPath, int messageId) {
+        sendPhoto(chatId, photoPath);
+        trackSentMessageId(chatId, messageId);
+    }
+
+    protected void replyAndTrack(Long chatId, String text, InlineKeyboardMarkup keyboard, int fallbackMessageId) {
+        reply(chatId, text, keyboard);
+        trackSentMessageId(chatId, fallbackMessageId);
+    }
+
+    protected void replyAndTrack(Long chatId, String text, int fallbackMessageId) {
+        reply(chatId, text);
+        trackSentMessageId(chatId, fallbackMessageId);
+    }
+
+    protected void trackSentMessageId(Long chatId, int candidateMessageId) {
+        if (messageIDKeeperService.hasMessageId(chatId)) {
+            Integer lastMessageId = messageIDKeeperService.getLastMessageId(chatId);
+            messageIDKeeperService.addMessageId(chatId, lastMessageId + 1);
+            log().info("Command: {} Tracked incremented messageId {} for chat {}", getClass().getSimpleName(), lastMessageId + 1, chatId);
+        } else {
+            messageIDKeeperService.addMessageId(chatId, candidateMessageId);
+            log().info("Command: {} Tracked fallback messageId {} for chat {}", getClass().getSimpleName(), candidateMessageId, chatId);
+        }
+    }
 
     protected abstract Logger log();
 
