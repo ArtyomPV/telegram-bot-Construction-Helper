875d023d0714a052af4d76c47569f88b09b0b309
Артем Васильевич Прусов <itcoding76@gmail.com>
2025-11-29 17:47:27 +0200

    Added a crutch to getHandler for contracts

--- src/main/java/ru/prusov/TelegramBotConstructionHelper/usecase/routers/CallbackRouter.java
+++ src/main/java/ru/prusov/TelegramBotConstructionHelper/usecase/routers/CallbackRouter.java
@@ -3,7 +3,12 @@ package ru.prusov.TelegramBotConstructionHelper.usecase.routers;
 import org.springframework.stereotype.Component;
 import ru.prusov.TelegramBotConstructionHelper.usecase.callback.CallbackCommand;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static ru.prusov.TelegramBotConstructionHelper.usecase.callback.CallbackData.CONTRACTS_PAGE;
 
 @Component
 public class CallbackRouter {
@@ -18,6 +23,9 @@ public class CallbackRouter {
     }
 
     public Optional<CallbackCommand> getHandler(String callbackDataCommand) {
+        if (callbackDataCommand.startsWith(CONTRACTS_PAGE)) {
+            callbackDataCommand = callbackDataCommand.substring(0, callbackDataCommand.length() - 1);
+        }
         return Optional.ofNullable(handlerMap.get(callbackDataCommand));
     }
 }
